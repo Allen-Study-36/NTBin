6befa9cbf583d70f02bb2da7d0b0d064fc51ee1c
GyuBin Hwang <spiraldailydev@gmail.com>
2024-10-30 19:23:31 +0900

    init, add ui component

---
+++ NTBin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NTBin
+//
+//  Created by gbstar on 10/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
