bb51a464538ea30ff43d34955c1a982b35a653da
GyuBin Hwang <spiraldailydev@gmail.com>
2024-10-30 19:54:16 +0900

    refactoring to make ui with code

---
+++ NTBin/FirstVC.swift
@@ -0,0 +1,18 @@
+//
+//  FirstVC.swift
+//  NTBin
+//
+//  Created by gbstar on 10/30/24.
+//
+
+import SwiftUI
+
+struct FirstVC: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FirstVC()
+}
---
+++ NTBin/ThirdViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ThirdViewController.swift
+//  NTBin
+//
+//  Created by gbstar on 10/30/24.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
